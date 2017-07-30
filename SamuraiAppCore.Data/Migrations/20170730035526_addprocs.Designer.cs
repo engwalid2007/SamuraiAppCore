3dc203a37e6a5a0770ffae15aadbe234c6db2230
Waleed Hammad <eng_walid2007@hotmail.com>
2017-07-30 18:52:09 +0300

    add proc

---
+++ SamuraiAppCore.Data/Migrations/20170730035526_addprocs.Designer.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using SamuraiAppCore.Data;
+
+namespace SamuraiAppCore.Data.Migrations
+{
+    [DbContext(typeof(SamuraiDataContext))]
+    [Migration("20170730035526_addprocs")]
+    partial class addprocs
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("SamuraiAppCore.Domains.Buttle", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("EndDate");
+
+                    b.Property<string>("Name");
+
+                    b.Property<DateTime>("StartDate");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Buttles");
+                });
+
+            modelBuilder.Entity("SamuraiAppCore.Domains.Quote", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.Property<int>("SamuraiId");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("SamuraiId");
+
+                    b.ToTable("Quotes");
+                });
+
+            modelBuilder.Entity("SamuraiAppCore.Domains.Samurai", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("CreationDate");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Samurais");
+                });
+
+            modelBuilder.Entity("SamuraiAppCore.Domains.SamuraiButtle", b =>
+                {
+                    b.Property<int>("ButtleId");
+
+                    b.Property<int>("SamuraiId");
+
+                    b.HasKey("ButtleId", "SamuraiId");
+
+                    b.HasIndex("SamuraiId");
+
+                    b.ToTable("SamuraiButtles");
+                });
+
+            modelBuilder.Entity("SamuraiAppCore.Domains.Warrior", b =>
+                {
+                    b.Property<int>("WarriorId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.Property<int>("SamuraiId");
+
+                    b.HasKey("WarriorId");
+
+                    b.HasIndex("SamuraiId")
+                        .IsUnique();
+
+                    b.ToTable("Warriors");
+                });
+
+            modelBuilder.Entity("SamuraiAppCore.Domains.Quote", b =>
+                {
+                    b.HasOne("SamuraiAppCore.Domains.Samurai", "Samurai")
+                        .WithMany("Quotes")
+                        .HasForeignKey("SamuraiId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("SamuraiAppCore.Domains.SamuraiButtle", b =>
+                {
+                    b.HasOne("SamuraiAppCore.Domains.Buttle", "Buttle")
+                        .WithMany()
+                        .HasForeignKey("ButtleId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("SamuraiAppCore.Domains.Samurai", "Samurai")
+                        .WithMany()
+                        .HasForeignKey("SamuraiId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("SamuraiAppCore.Domains.Warrior", b =>
+                {
+                    b.HasOne("SamuraiAppCore.Domains.Samurai", "Samurai")
+                        .WithOne("Warrior")
+                        .HasForeignKey("SamuraiAppCore.Domains.Warrior", "SamuraiId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ SamuraiAppCore.Data/Migrations/20170730035526_addprocs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SamuraiAppCore.Data.Migrations
+{
+    public partial class addprocs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("" +
+                "CREATE PROC GETSAMURAIS @NAME NVARCHAR(50) " +
+                "AS SELECT * FROM SAMURIS WHERE NAME LIKE '%'+@NAME+'%'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
